ef151eea574e0ddbcc8f4365ba2f015b68bf47d7
irenevoskamp <irene@voskamp.org>
2017-09-24 16:34:07 -0700

    Update MLProject4.R

--- MLProject4.R
+++ MLProject4.R
@@ -104,14 +104,57 @@ predValidRF <- predict(modFitrf, validation)
 
 confus <- confusionMatrix(predValidRF, validation$classe)
 confus
+Confusion Matrix and Statistics
 
-#Reference
+#          Reference
 #Prediction    A    B    C    D    E
-#A  1391    2    0    0    2
-#B      2  946    1    0    0
-#C     0    2  852    1    0
-#D     0    0    4  800    0
-#E     0    0    0    3  898
+#         A 1391    2    0    0    2
+#         B    2  946    1    0    0
+#         C    0    2  852    1    0
+#         D    0    0    4  800    0
+#         E    0    0    0    3  898
+
+#Overall Statistics
+                                         
+#               Accuracy : 0.9965         
+#                 95% CI : (0.9945, 0.998)
+#    No Information Rate : 0.2841         
+#    P-Value [Acc > NIR] : < 2.2e-16      
+                                         
+#                  Kappa : 0.9956         
+# Mcnemar's Test P-Value : NA             
+
+#Statistics by Class:
+
+#                     Class: A Class: B Class: C Class: D Class: E
+#Sensitivity            0.9986   0.9958   0.9942   0.9950   0.9978
+#Specificity            0.9989   0.9992   0.9993   0.9990   0.9993
+#Pos Pred Value         0.9971   0.9968   0.9965   0.9950   0.9967
+#Neg Pred Value         0.9994   0.9990   0.9988   0.9990   0.9995
+#Prevalence             0.2841   0.1937   0.1748   0.1639   0.1835
+#Detection Rate         0.2836   0.1929   0.1737   0.1631   0.1831
+#Detection Prevalence   0.2845   0.1935   0.1743   0.1639   0.1837
+#Balanced Accuracy      0.9987   0.9975   0.9967   0.9970   0.9985
+
+modFitrf
+#Random Forest 
+
+#14718 samples
+#   27 predictor
+#    5 classes: 'A', 'B', 'C', 'D', 'E' 
+
+#No pre-processing
+#Resampling: Cross-Validated (10 fold) 
+#Summary of sample sizes: 13246, 13247, 13245, 13246, 13246, 13247, ... 
+#Resampling results across tuning parameters:
+
+#  mtry  Accuracy   Kappa    
+#   2    0.9927302  0.9908040
+#  14    0.9923222  0.9902882
+#  27    0.9882454  0.9851323
+
+#Accuracy was used to select the optimal model using  the largest value.
+#The final value used for the model was mtry = 2. 
 
 #We see there are very few variables out of this model.
 
@@ -134,6 +177,7 @@ out_of_sample_error
 
 pred_final <- predict(modFitrf, pre_testingDataSet)
 pred_final
+
 
 #[1] B A B A A E D B A A B C B A E E A B B B
 #Levels: A B C D E
